cd95398af805f7bbf429acdb6861e4f90b96faae
charles <yaogci2002@gmail.com>
2025-04-26 15:49:08 +0000

    new commit link 10

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ const App = () => {
 
   useEffect(function(){
     if(cart_code){
-      api.get(`get_cart_stat?cart_code=${cart_code}`)
+      api.get(`get_cart_stat/?cart_code=${cart_code}`)
       .then(res =>{
         console.log(res.data)
         setNumberCartItems(res.data.num_of_items)
