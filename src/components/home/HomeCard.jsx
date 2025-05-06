54e66abec668b92da481a43565770cb7f7752206
charles <yaogci2002@gmail.com>
2025-05-06 14:27:17 +0000

    last commit 2

--- src/components/home/HomeCard.jsx
+++ src/components/home/HomeCard.jsx
@@ -1,24 +1,32 @@
 import React from 'react'
 import styles from './HomeCard.module.css'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { BASE_URL } from '../../api'
 
-const HomeCard = ({product}) => {
+const HomeCard = ({ product }) => {
   return (
     <div className={`col-md-3 ${styles.col}`}>
-    <Link to={`/products/${product.slug}`} className={styles.link}>
-    <div className={styles.card}>
-        <div className={styles.cardImgWrapper}>
-            <img src={`${BASE_URL}${product.image}`} alt="Product Image" 
-            className={styles.cardImgTop}
+      <Link to={`/products/${product.slug}`} className={styles.link}>
+        <div className={styles.card}>
+          <div className={styles.cardImgWrapper}>
+            <img
+              src={`${BASE_URL}${product.image}`}
+              alt="Product Image"
+              className={styles.cardImgTop}
             />
-        </div>
-        <div className={styles.cardBody}>
+            {/* Ajout de la description au survol */}
+            {product.description && (
+              <div className={styles.imageOverlay}>
+                <p className={styles.imageDescription}>{product.name}<br />{product.price} Fcfa</p>
+              </div>
+            )}
+          </div>
+          <div className={styles.cardBody}>
             <h5 className={`${styles.cardTitle} mb-1`}>{product.name}</h5>
             <h6 className={styles.cardText}>{`${product.price} FCFA`}</h6>
+          </div>
         </div>
-      </div>
-    </Link>
+      </Link>
     </div>
   )
 }
